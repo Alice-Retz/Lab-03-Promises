cd68b7740c9e9763d48dfb81d3f7bdb22a1c5647
Alice-Retz <alicegretz@gmail.com>
2021-09-15 17:01:59 -0500

    First test set up.

---
+++ __tests__/simpleDb.test.js
@@ -0,0 +1,25 @@
+import { rm, mkdir } from 'fs/promises';
+import { createId } from '../simpleDb.js';
+
+describe('Routes for simple-db api', () => {
+  // create a link to destination folder
+  // set up beforeEach to have a clean working slate
+  // First test: does saved object have id?
+  // Write the function
+  // Second test: save and get objects
+  // Write the function
+
+  const destination = './__tests__/dest/';
+
+  beforeEach(() => {
+    return rm(destination, { force: true, recursive: true }).then(() => {
+      return mkdir(destination);
+    });
+  });
+
+  it('should check if a saved object has an id', () => {
+    const checkID = new createId(destination);
+
+    return checkID;
+  });
+});
---
+++ simpleDb.js
@@ -0,0 +1,9 @@
+import path from 'path';
+import shortid from 'shortid';
+
+export class createId {
+  constructor(destination) {
+    const filename = `${shortid.generate().txt}`;
+    this.newFile = path.join(destination, filename);
+  }
+}
